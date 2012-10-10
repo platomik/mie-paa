500ffa6dc851914993e28ef9f10adefa78d9a91e
Mikhail Platonov <platomik@fit.cvut.cz>
2012-10-10 18:14:15 +0200

    new file:   2/bu.inst.dat 	new file:   2/ett.pl 	new file:   2/fem.pl 	new file:   2/fyra.pl 	new file:   2/tre.pl 	new file:   2/tva.pl

---
+++ 2/fem.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+@V = (6, 7, 8, 9);	#capacities
+@T = (5, 6, 5, 5);		#target
+@S = (5, 5, 5, 5);		#initial
+@cur=(0, 0, 0, 0);		#current
+$n = 4;
+
+#@cur=@S;
+
+sub go_down {
+ my(@cur) = @_;	
+# print @cur; print "\n\n";
+  for ($i=0; $i<$n; $i++){
+
+	#fill bucket one by one
+	$left=$cur[$i];
+	$cur[$i]=$V[$i];
+#	print @cur;	print "\n";
+	unshift(@Q, @cur);	
+	
+	#empty bucket one by one
+	$cur[$i]=0;
+#	print @cur;	print "\n";
+	unshift(@Q, @cur);
+	$cur[$i]=$left;
+	
+	#pour a water from left bucket to right bucket
+	for ($ii=0; $ii<$n-1; $ii++){
+		$left_id=$i;
+		$right_id=($i+$ii+1)%($n);
+		$left=$cur[$left_id];
+		$right=$cur[$right_id];
+		
+		if ($V[$right_id]-$left-$right>=0) {
+			$cur[$right_id]=$left+$right;
+			$cur[$left_id]=0;
+		}else{
+			$cur[$right_id]=$V[$right_id];
+			$cur[$left_id]=$left+$right-$V[$right_id];
+		}
+#		print @cur;	print "\n";
+		unshift(@Q, @cur);
+		$cur[$left_id]=$left;
+		$cur[$right_id]=$right;
+	}
+
+  }
+}
+
+go_down(@S);
+
+print @Q;
+print "\n\n";
+print pop @Q;
+print pop @Q;
+print pop @Q;
+print pop @Q;
