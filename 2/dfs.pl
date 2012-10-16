232c56de42f1599c19d6093d657b792caa417e04
Mikahil Platonov <platomik@fit.cvut.cz>
2012-10-16 02:42:54 +0200

    dfs-ready

--- 2/dfs.pl
+++ 2/dfs.pl
@@ -1,16 +1,21 @@
 #!/usr/bin/perl
 
-@V = (15,12,8,4,6);	 	#capacities
-@T = (5,5,5,0,1);		#target
-@S = (0,0,0,0,0);		#initial
-#@cur=(0, 0, 0, 0);		#current
+@V = (14, 10, 6, 2, 8);	 	#capacities
+@T = (12, 6, 4, 1, 8);		#target
+@S = (0, 0, 1, 0, 0);		#initial
+
+#@V = (3, 2, 3);	 	#capacities
+#@T = (0, 1, 0);		#target
+#@S = (1, 1, 1);		#initial
+
+
 $n = $#S+1;
 
 #here is just array printing
 sub print_arr{
 	my(@AoA) = @_;
 	for (my $i = 0; $i <= $#AoA; $i++) {
-		print $i+1, ". ";
+#		print $i+1, ". ";
 	   for (my $j = 0; $j <= $#{$AoA[$i]}; $j++) {
     	  print $AoA[$i][$j], " "; 
     	}
@@ -25,9 +30,19 @@ sub is_solution {
     	  if ( $T[$i] == $AoA[$i]) { $ok=1; } else { $ok=2; last; }; 
 	}
 	if ($ok==1) {
-		unshift(@Q, [@AoA]);
-		print "we found a solution\n"; 
-#		print_arr(@Q);
+		push(@Q, [@AoA]);
+	   	push(@R, [$#Q,$id]);		
+		print "We have found a solution after visiting ",$#Q+1," nodes!\n"; 
+		print "Solution tree looks like: \n";
+#		print_arr(@R);
+		$child=$#Q;
+		while ($father ne "Null") {
+			$cc++;
+			print_arr($Q[$child])." ";
+			$father=$R[$child][1];
+			$child=$father;
+		}
+		print "Depth of the solution tree is: $cc \n";
 		exit;
 	}
 }
@@ -40,7 +55,10 @@ sub is_duplicate{
 		if ("@y" eq "@{$Q[$i]}") { $dup=$i+1;}
     }
    if ($dup==0) { 
-	   	unshift(@Q, [@y]);
+#   	print @y,"\n";
+	   	push(@Q, [@y]);
+#	   	print "father: ".$id,"; child: ", $#Q,"\n";
+	   	push(@R, [$#Q,$id]);
    }    
 
 }
@@ -48,6 +66,7 @@ sub is_duplicate{
 # n^2+n branches from a bud
 sub go_down {
  my(@cur) = @_;	
+ @father=@cur;
 # print @cur; print "\n\n";
   for ($i=0; $i<$n; $i++){
 
@@ -88,10 +107,13 @@ sub go_down {
 
 @Q=[@S];	#set root element from the initial array
 
-for (my $i = 0; $i <= $#Q; $i++) {
-#	print @{$Q[$#Q-$i]},"\n";
-	print $i+1,"\n";
-	go_down(@{$Q[0]});
-}
+push(@R, [0,"Null"]); # format (father;child)
 
-print_arr(@Q);	
+for (my $i = 0; $i <= $#Q; $i++) {
+#	print "element ",@{$Q[$#Q-$i]},"\n";
+#	print "element ",@{$Q[$#Q]},"\n";
+	print " ",$i+1,"\n";
+	$id=$#Q;
+	go_down(@{$Q[$#Q]});
+}
+print "\n";
